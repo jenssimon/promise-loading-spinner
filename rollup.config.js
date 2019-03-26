c6801a01c82d47ac1e22abb594e1ede2b8be3652
Jens Simon <jens.simon80@googlemail.com>
2019-03-27 00:49:58 +0100

    Build bundles

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'src/loader.js',
+    output: {
+      name: 'rollupJestBoilerplate',
+      file: pkg.browser,
+      format: 'umd',
+    },
+    plugins: [
+      resolve(),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      commonjs(),
+    ],
+  },
+
+  {
+    input: 'src/loader.js',
+    external: [],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+      }),
+    ],
+  },
+];
